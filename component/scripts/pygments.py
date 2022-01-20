b0048d1d79a4b9054ba4c70ad93c73d0737cf65a
12rambau <pierrick.rambaud49@gmail.com>
2022-01-20 16:34:57 +0000

    improve display of the code

--- component/scripts/pygments.py
+++ component/scripts/pygments.py
@@ -13,4 +13,11 @@ def html(raw_code):
     lex = lexers.get_lexer_by_name("javascript")
     formated_code = highlight(raw_code, lex, formatter)
 
+    # add some styling to the initial div tag
+    html_style = "overflow: auto; max-height: 65vh; border-radius: 3px; border: 1px solid lightgrey;"
+    old_tag = '<div class="highlight">'
+    new_tag = f'<div class="highlight pa-3 mt-2" style="{html_style}">'
+
+    formated_code = formated_code.replace(old_tag, new_tag)
+
     return formated_code
